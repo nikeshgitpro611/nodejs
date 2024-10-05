30818be984a409a0ad4b8d5e9888a73e86a946d9
NIKESH KUMAR <nikesh.kumar7@ibm.com>
2024-10-05 21:37:55 +0530

    StoreAPI

--- John/StoreApi/controller/product.js
+++ John/StoreApi/controller/product.js
@@ -1,27 +1,59 @@
 const productSchemaDefine = require("../modals/productSchemaDefine");
 
 const getAllProduct = async (req, res) => {
-  const { featured, name, company, rating, sort } = req.query;
+  console.log(req.query.page);
+
+  const { featured, name, company, rating, sort, fields, numericFilters } =
+    req.query;
   const queryObject = {
-    ...(featured && { featured: featured === "true" }),
-    ...(name && { name: { $regex: name, $options: "i" } }),
-    ...(company && { company }),
-    ...(rating && { rating }),
+    // ...(featured && { featured: featured === "true" }),
+    // ...(name && { name: { $regex: name, $options: "i" } }),
+    // ...(company && { company }),
+    // ...(rating && { rating }),
   };
 
   console.log("queryObject : ", queryObject);
 
-  // if (featured) {
-  //   queryObject.featured = featured === "true" ? true : false;
-  // }
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
 
-  // if (name) {
-  //   queryObject.name = name;
-  // }
+  if (name) {
+    queryObject.name = name;
+  }
 
-  // if (company) {
-  //   queryObject.company = company;
-  // }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (rating) {
+    queryObject.company = rating;
+  }
+  if (numericFilters) {
+    let filterMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${filterMap[match]}-`
+    );
+    const option = ['price', 'rating'];
+    filters.split(',').forEach(element => {
+      const [field,oprater,value] = element.split("-");
+      if (option.includes(field)) {
+        queryObject[field] = {[oprater]: value}
+      }
+    });
+    console.log("filters : ", filters);
+    console.log("queryObject : ", queryObject);
+
+    // queryObject.company = rating;
+  }
 
   //Based on Query
   let data = productSchemaDefine.find(queryObject);
@@ -29,13 +61,29 @@ const getAllProduct = async (req, res) => {
     const sortList = sort.split(",").join(" ");
     // console.log(sort.split(',').join(' '));
     data = data.sort(sortList);
+  } else {
+    data = data.sort("createdAt");
   }
+
+  //Sorting Particular Fields
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    data = data.select(fieldList);
+  }
+
+  //Pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+  data = data.skip(skip).limit(limit);
+
   const products = await data.lean();
 
   res.status(200).json({
     status: true,
     message: "Products Route all Data...",
-    nbHits: data.length,
+    nbHits: products.length,
     data: products,
   });
 };
--- John/TASKMENAGERAPI/readme.md
+++ John/TASKMENAGERAPI/readme.md
@@ -70,6 +70,24 @@ let data = productSchemaDefine.find(queryObject);
   const products = await data.lean(); 
 
   >: If you're working with MongoDB, use .lean() to return plain JavaScript objects instead of Mongoose documents. This removes any circular references and allows you to safely manipulate and send the data in JSON format.
+
+  //Sorting Particular Fields
+  if (fields) {
+    const fieldList = fields.split(',').join(' ')
+    data = data.select(fieldList)
+  }
+
+   //PAGINATION
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+  data = data.skip(skip).limit(limit)
+
+  const products = await data.lean();
+
+  //USER CAN SEARCH ON BASED On NUMBER CONDITION...!!!
+
 ```
 
 # Task Schema
